4acca39dfd3bbb6c9a6247f51ab79808b9ca934a
ffdietz <felipefierrod@gmail.com>
2021-10-12 03:25:40 +0200

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -14,14 +14,20 @@
 // programming
 
 void setup() {
-  // Serial.begin(9600);
-  sd_init();
+  Serial.begin(9600);
+  // sd_init();
   //pinMode PWM OUTPUT
-  pinMode(13, OUTPUT);
+  pinMode(3, OUTPUT);
 }
 
+byte state = 0;
 void loop() {
   // sd read
   // millis delay( analogRead )
   //  analogWrite
-  }
+  analogWrite(3, 64 + analogRead(1)/4); //minimun motor signal
+  delay(500);
+  analogWrite(3, 0);
+  delay(500);
+  Serial.println(analogRead(1)/4);
+}
